d990be6cd96f146278d3b8696a2789085dd7b373
Mond <herecomesthemoon@protonmail.com>
2022-07-15 10:23:13 +0200

    Fixed 5.py

--- 5.py
+++ 5.py
@@ -1,22 +1,23 @@
-import csv
 from collections import namedtuple
 
 Point = namedtuple('Point', 'x y')
 
 def read():
     with open('./data/5.csv') as f:
-        r = csv.reader(f)
-        data = [(Point(int(x[0]), int(x[1])), Point(int(x[2]), int(x[3]))) for x in r]
+        r = f.readlines()
+        rows = [row.replace(" -> ", ",") for row in r]
+        rows = [row.split(",") for row in rows]
+        data: list[tuple[Point, Point]] = [(Point(int(x[0]), int(x[1])), Point(int(x[2]), int(x[3]))) for x in rows]
     return data
 
-def line(p, q):
+def line(p: Point, q: Point) -> list[tuple[int, int]]:
     if p.x == q.x:
         return [(p.x, z) for z in range(min(p.y, q.y), max(p.y, q.y)+1)]
     if p.y == q.y:
         return [(z, p.y) for z in range(min(p.x, q.x), max(p.x, q.x)+1)]
     return []
 
-def line2(p, q):
+def line2(p: Point, q: Point) -> list[tuple[int, int]]:
     if p.x == q.x:
         return [(p.x, z) for z in range(min(p.y, q.y), max(p.y, q.y)+1)]
     if p.y == q.y:
@@ -27,8 +28,9 @@ def line2(p, q):
         return [(p.x - k*d.x, p.y - k*d.y) for k in range(0, abs(p.x - q.x) + 1)]
 
     return []
+
 def first():
-    karte = [[0 for _ in range(1000)] for _ in range(1000)]
+    karte = [[0 for _ in range(1000)] for _ in range(1000)] # 1000 ~ bound for highest appearing value.
     for x, y in read():
         for a, b in line(x, y):
             karte[a][b] += 1
@@ -36,7 +38,7 @@ def first():
     count = 0
     for row in karte:
         for column in row:
-           count += column >= 2
+           count += (column >= 2)
 
     return count
 
@@ -49,8 +51,7 @@ def second():
     count = 0
     for row in karte:
         for column in row:
-            count += column >= 2
+            count += (column >= 2)
 
     return count
 
-print(second())
