04b517daeb4e2a167ecb70b4e28897289c38c387
Mond <herecomesthemoon@protonmail.com>
2022-07-16 10:21:32 +0200

    Fixed 13.py

--- 13.py
+++ 13.py
@@ -1,27 +1,27 @@
-import csv
+def read(loc: str) -> tuple[list[tuple[int, int]], list[tuple[str, int]]]:
+    with open(loc) as f:
+        r = f.readlines()
+        split = r.index("\n")
+        points, folds = r[:split], r[split+1:]
 
+        points = [row.split(",") for row in points]
+        points = [(int(row[0]), int(row[1])) for row in points]
 
-def read():
-    with open('./data/13a.csv') as f:
-        r = csv.reader(f)
-        points = [(int(x), int(y)) for x, y in r]
-
-    with open('./data/13b.csv') as f:
-        r = csv.reader(f, delimiter='=')
-        folds = [(axis, int(pos)) for axis, pos in r]
+        folds = [row[11:].split("=") for row in folds]
+        folds = [(axis, int(pos)) for axis, pos in folds]
 
     return points, folds
 
 
-def fold(grid: list[list[bool, ...], ...], axis: str, pos: int) -> list[list[bool, ...], ...]:
+def fold(grid: list[list[bool]], axis: str, pos: int) -> list[list[bool]]:
     if axis == 'y':
-        print(f"Dimensions:, y:{len(grid)}, x:{len(grid[0])}")
+        # print(f"Dimensions:, y:{len(grid)}, x:{len(grid[0])}")
         poss = [(pos, k) for k, x in enumerate(grid[0])]
-        print(poss)
+        # print(poss)
         new_grid = [[False for _ in range(len(grid[0]))] for _ in range(pos)]
         for y, x in poss:
             matches = [((y-k, x), (y+k, x)) for k in range(1, pos+1)]
-            print(matches)
+            # print(matches)
             try:
                 for a, b in matches:
                     new_grid[a[0]][a[1]] = grid[a[0]][a[1]] or grid[b[0]][b[1]]
@@ -29,7 +29,8 @@ def fold(grid: list[list[bool, ...], ...], axis: str, pos: int) -> list[list[boo
                 pass
         return new_grid
 
-    if axis == 'x':
+    else:
+        assert axis == 'x'
         grid = list(map(list, zip(*grid)))
         new_grid = fold(grid, 'y', pos)
         new_grid = list(map(list, zip(*new_grid)))
@@ -38,7 +39,7 @@ def fold(grid: list[list[bool, ...], ...], axis: str, pos: int) -> list[list[boo
 
 
 
-def build_grid(points: list[tuple[int, int], ...]) -> list[list[bool, ...], ...]:
+def build_grid(points: list[tuple[int, int]]) -> list[list[bool]]:
     n = max(points, key=lambda p: p[0])[0]
     m = max(points, key=lambda p: p[1])[1]
     grid = [[False for _ in range(n + 1)] for _ in range(m + 1)]
@@ -47,7 +48,7 @@ def build_grid(points: list[tuple[int, int], ...]) -> list[list[bool, ...], ...]
     return grid
 
 
-def print_grid(grid: list[list[bool, ...], ...]):
+def print_grid(grid: list[list[bool]]):
     if not grid:
         grid = [[]]
     prnt = ""
@@ -62,31 +63,21 @@ def print_grid(grid: list[list[bool, ...], ...]):
     print(f"Number of dots: {prnt.count('#')}")
 
 
-def first(points: list[tuple[int, int], ...], folds: list[tuple[str, int]]):
+def first(loc: str = "./data/13.csv"):
+    points, folds = read(loc)
     grid = build_grid(points)
-    # print_grid(grid)
-    print(folds)
-
     grid = fold(grid, folds[0][0], folds[0][1])
     print_grid(grid)
-    # for axis, pos in folds:
-    #     print(axis, pos)
-    #     grid = fold(grid, axis, pos)
-        # print_grid(grid)
 
 
-
-def second(points: list[tuple[int, int], ...], folds: list[tuple[str, int]]):
+def second(loc: str = "./data/13.csv"):
+    points, folds = read(loc)
     grid = build_grid(points)
-    # print_grid(grid)
-    print(folds)
-
     for axis, pos in folds:
         grid = fold(grid, axis, pos)
     print_grid(grid)
 
 
-
-second(*read())
-
+first()
+second()
 
