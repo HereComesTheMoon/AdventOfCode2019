752c8356b0a257438baa36919eac83185e572a83
Mond <herecomesthemoon@protonmail.com>
2022-07-17 18:38:16 +0200

    Cleaned up 17.py

--- 17.py
+++ 17.py
@@ -1,34 +1,29 @@
 import math
+from collections import namedtuple
 
-X_MIN = 124
-X_MAX = 174
-Y_MIN = -123
-Y_MAX = -86
-#
-# X_MIN = 20
-# X_MAX = 30
-# Y_MIN = -10
-# Y_MAX = -5
+target = namedtuple('Target', 'x_min x_max y_min y_max')
 
-# Important/obvious:
-# Init velocity (x, y), at starting position (0, 0)
-# Assuming y > 0 : The probe will be at height 0 after 2y steps
-# After 2Y steps the probe will be exactly on height 0.
-# This leaves two questions:
-# 1. What's the biggest downwards trajectory at 0 so that we're still in the target?
-# 2. For which value of X are we going to hit the target?
+t: tuple[int, int, int, int] = target(0,1,2,3)
 
-def upd_vel(dx: int, dy: int):
-    # Technically dx always positive in part 1, so this could be optimised.
-    if dx == 0:
-        return 0, dy-1
-    return min(abs(dx-1), abs(dx+1)), dy-1
+
+# This was way more work than it should have been. Trying to be clever was a bad idea. This here was severely cleaned up, and it is still not pretty.
+
+
+def read(loc: str) -> tuple[int, int, int, int]:
+    with open(loc) as f:
+        r = f.readline()[15:-1]
+        x_part, y_part = r.split(", y=")
+        x_min, x_max = map(int, x_part.split(".."))
+        y_min, y_max = map(int, y_part.split(".."))
+        t: tuple[int, int, int, int] = target(x_min, x_max, y_min, y_max)
+        return t
 
 
 def peak_y(dy: int):
     if dy <= 0:
         return 0
     return dy * (dy + 1) // 2
+
 
 def x_after_k_steps(dx:  int, steps: int):
     assert dx >= 0
@@ -40,127 +35,70 @@ def x_after_k_steps(dx:  int, steps: int):
     else:
         return dx * (dx + 1) // 2 - (dx - steps) * (dx - steps + 1) // 2
 
-def trajectory(x: int, y: int):
-    pos_x, pos_y = x, y
-    k = 1
-    while pos_y > Y_MIN - 2*abs(Y_MIN - Y_MAX):
-        x, y = upd_vel(x, y)
-        print(f"Step: {k}. Position: ({pos_x}, {pos_y}) Target: {X_MIN <= pos_x <= X_MAX and Y_MIN <= pos_y <= Y_MAX}. Trajectory: ({x}, {y})")
-        pos_x += x
-        pos_y += y
-        k += 1
 
-    print(f"Step: {k}. Position: ({pos_x}, {pos_y}). Trajectory: ({x}, {y})")
+def upd_vel(dx: int, dy: int):
+    return min(abs(dx-1), abs(dx+1), abs(dx)), dy-1
 
 
-def x_min_steps(x: int):
-    # for x in range(math.isqrt(2 * x_min), math.isqrt(2 * x_max)):  # Approximations. Culls unnecessary cases
-    pos = 0
-    dx = x
-    for k in range(1, x+1):
-        pos += dx
-        dx = max(dx - 1, 0)
-        if pos < X_MIN:
-            continue
-        if X_MAX < pos:
-            return None
-        else:
-            return k
-
-def y_range(y: int):
-    k = 0
-    dy = y
-    pos = 0
-    if y > 0:
-        k = 2*y +1
-        dy = - y - 1
-
-    while pos > Y_MAX:
-        print(f"{pos=}")
-        pos += dy
-        dy -= 1
-        k += 1
-
-    k_min = k
-    print(f"{k_min=}")
-
-    while pos >= Y_MIN:
-        print(f"{pos=}")
-        pos += dy
-        dy -= 1
-        k += 1
-
-    k_max = k
-
-    return range(k_min, k_max+1)
+def is_sol(t: target, dx: int, dy: int) -> bool:
+    pos_x, pos_y = 0, 0
+    while pos_y > t.y_min and pos_x <= t.x_max:
+        pos_x += dx
+        pos_y += dy
+        dx, dy = upd_vel(dx, dy)
+        if dx == 0 and pos_x < t.x_min:
+            return False
+        if t.x_min <= pos_x <= t.x_max and t.y_min <= pos_y <= t.y_max:
+            return True
+    return False
 
 
-def x_vals(x_min: int, x_max: int):
-    # math.isqrt(2*x_max) + 1 approximation, always picks a starting value high enough that probe overshoots
-    # idea: Lower x and compute all possible x values, and their respective step ranges
-    for x in range(math.isqrt(2*x_max)+1, 0, -1):
-        for k in range(x, 0, -1):
-            if x_min <= x_after_k_steps(x, k) <= x_max:
-                pass
-
-
-
-
-def first(x_min: int, x_max: int, y_min: int, y_max: int):
-    """I am super tired today, so this is really bad. But hey, it works."""
-    y = abs(y_min) -1
-    while True:
-        for x in range(math.isqrt(2*x_min), math.isqrt(2*x_max)): # Approximations. Culls unnecessary cases
-            if x_min <= x_after_k_steps(x, 2*y+1) <= x_max:
-                return x, y
-        y += 1
-
-
-def second():
-    """I am super tired today, so this is really bad. But hey, it works."""
+def find_all_solutions(t: target) -> list[tuple[int,int]]:
     solutions = []
-    for y in range(Y_MIN, -Y_MIN):
-        for y_now in y_range(y):
-            for x in range(math.isqrt(2 * X_MIN)-6, math.isqrt(2 * X_MAX)+200):
-                if X_MIN <= x_after_k_steps(x, y_now) <= X_MAX:
-                    solutions.append((x, y_now))
-    return solutions
 
-def second2():
-    solutions = []
-    for y in range(Y_MIN, -Y_MIN):
-        for x in range(X_MAX+1):
-            if is_sol(x, y):
+    # These bounds for x and y are tight
+    for y in range(t.y_min, -t.y_min):
+        for x in range(math.isqrt(2 * t.x_min), t.x_max + 1):
+            if is_sol(t, x, y):
                 solutions.append((x,y))
 
     return solutions
 
 
-def is_sol(dx: int, dy: int):
-    pos_x, pos_y = 0, 0
-    while pos_y > Y_MIN:
-        pos_x += dx
-        pos_y += dy
-        dx, dy = upd_vel(dx, dy)
-        if X_MIN <= pos_x <= X_MAX and Y_MIN <= pos_y <= Y_MAX:
-            return True
-    return False
+def first(t: target) -> int:
+    """This is overly convoluted, and unnecessary. Keeping it here for authenticity. To explain why this works. Starting values are (x,y), where y > 0. We also assume that y_max is negative. After y steps the probe (following a parabolic arc) will have reached its peak, after 2y steps it will be at height 0, with a downwards velocity of -y. At the next step it will be at y-position -y, which has to be contained in our target area. In other words, y = abs(y_min) - 1 is the best possible starting y-velocity which we can achieve. We now need to check whether we can find an x, such that x_after_k_steps is within the target bounds for x after exactly 2y+1 steps. If this is possible at all, then we are done. This is the case for my example. This task does not specify much about the type of area. Changing or moving the area drastically changes how this problem has to be approached.
+
+    Our assumptions: y_max < 0. It is possible to find a solution such that the probe is in the area after exactly 2y+1 steps.
+
+    Side note: A higher value of y is impossible, it will always overshoot."""
+    for y in range(abs(t.y_min) - 1, abs(t.y_max) - 1, -1):
+        for x in range(math.isqrt(2*t.x_min), math.isqrt(2*t.x_max)): # Approximations. Culls unnecessary cases
+            if t.x_min <= x_after_k_steps(x, 2*y+1) <= t.x_max:
+                return peak_y(y)
+
+    assert False
 
 
+def first2(t: target) -> int:
+    sols = find_all_solutions(t)
+    maxi = 0
+    for _, y in sols:
+        maxi = max(maxi, peak_y(y))
 
-#
-# x, y = first(X_MIN, X_MAX, Y_MIN, Y_MAX)
-#
-# print(peak_y(y))
-#
-# trajectory(x, y)
-#
-#
-#
-# test = [(7,2), (6,3), (9,0 ), (17, -4)]
-# for x, y in test:
-#     print(list(y_range(y)))
-#     trajectory(x, y)
+    print(maxi)
+    return maxi
 
-sec = second2()
-print(len(sec))
+
+def second(t: target) -> int:
+    sols = find_all_solutions(t)
+    res = len(sols)
+    print(res)
+    return res
+
+
+t = read("./data/17.csv")
+
+
+first2(t)
+second(t)
+
